877c74c5b69230fe8b0c32ba9f5084675b77b5ef
eugenelijondo <eugene.lijondo@student.moringaschool.com>
2024-03-29 10:56:45 +0300

    first commit

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.condition = "New"
+
+    def cobble(self):
+        return "The shoe has been repaired."
+pass
